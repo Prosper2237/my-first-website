5a12c42f22565c15cf4ee9975af746d0dc6e4cf0
Prosper2237 <adogbeji.prosper@gmail.com>
2025-08-12 05:56:55 -0700

    Add dynamic text change on button click

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById('welcome-btn').addEventListener('click', function() {
     alert('Welcome to my site!');
 });
+
+document.getElementById('welcome-btn').addEventListener('click', function(){
+    document.getElementById('message').textContent = "You just clicked the magic button!";
+});
