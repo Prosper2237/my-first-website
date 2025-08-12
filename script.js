d2d7dc00bd969fc7387e3150c110a12ea454cf6b
Prosper2237 <adogbeji.prosper@gmail.com>
2025-08-12 06:33:41 -0700

    Add css styling and fade-in dynamic message

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+// document.getElementById('welcome-btn').addEventListener('click', function(){
+//     alert('Welcome to my site!');
+// });
+
+// 
+// document.getElementById('welcome-btn').addEventListener('click', function () {
+//     document.getElementById('message').textContent = "You just clicked the magic button!";
+// });
 
 
 document.getElementById('welcome-btn').addEventListener('click', function () {
-    document.getElementById('message').textContent = "You just clicked the magic button!";
+    const msg =  document.getElementById('message');
+    msg.textContent = "You just clicked the magic button!";
+    msg.style.opacity = 0;
+    setTimeout(() => {
+        msg.style.transition = 'opacity 0.5s';
+        msg.style.opacity = 1;
+    }, 50);
 });
